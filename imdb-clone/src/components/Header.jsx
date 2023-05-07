9ec66be1478c7d3a69caf390eb8764678a22cb09
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-07 12:52:42 -0700

    created the header component
    -installed the react icons package
    -created the menuItems conponent
    -created the header component and added it to the layout file

---
+++ imdb-clone/src/components/Header.jsx
@@ -0,0 +1,26 @@
+// 'use client'
+
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import {AiFillHome} from 'react-icons/ai';
+import {HiInformationCircle} from 'react-icons/hi';
+
+
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between mx-2 max-w-6xl sm:mx-auto py-2 sm:py-6">
+        <div className="flex items-center"> 
+         <MenuItem title='Home' address='/' Icon={AiFillHome}  />
+         <MenuItem title='About' address='/about' Icon={HiInformationCircle}/>
+         </div>
+        <div className=""> 
+            <Link href='/'>
+                <h2 className="text-2xl ">
+                    <span className=" mr-1 font-bold bg-amber-500 rounded-lg py-1 px-2">IMDb</span>
+                    <span className="text-xl hidden sm:inline">Clone</span>
+                </h2>
+            </Link>
+        </div>
+    </div>
+  )
+}
---
+++ imdb-clone/src/components/MenuItem.jsx
@@ -0,0 +1,14 @@
+// 'use client'
+
+import Link from "next/link";
+
+export default function MenuItem({title,address,Icon}) {
+  return (
+    <div>
+        <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600 ">
+            <p className="hidden sm:inline my-2 text-sm">{title}</p>
+            <Icon className='text-2xl sm:hidden mx-4'/>
+        </Link>
+    </div>
+  )
+}
