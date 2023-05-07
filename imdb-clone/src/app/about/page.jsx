2fe9da886979e7f863566d730c9e173bfbeaa3e1
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-07 15:12:18 -0700

    created the about page

---
+++ imdb-clone/src/app/about/page.jsx
@@ -0,0 +1,15 @@
+
+export default function About() {
+  return (
+    <div  className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl text-center font-medium text-amber-600">About</h1>
+        <p className="px-4">Welcome to our movie website, the ultimate destination for film enthusiasts! We are passionate about movies, and our goal is to provide you with the latest news, reviews, trailers, and everything else related to the world of cinema. </p>
+        <p className="px-4">Our team of dedicated writers and editors are movie lovers just like you, and we are committed to bringing you the most up-to-date information on the latest releases, as well as classic films that have stood the test of time. Whether you're a fan of action, romance, drama, or comedy, we've got you covered.</p>
+        <p className="px-4">Our website is designed to be user-friendly, with a clean and easy-to-navigate interface that makes finding the information you need a breeze. You can browse our extensive database of movies, read in-depth reviews, watch trailers, and even purchase tickets to the latest releases.
+
+We also feature a vibrant community of movie lovers who come together to discuss their favorite films, share recommendations, and engage in lively debates. We believe that movies have the power to connect people from all walks of life, and we're thrilled to be a part of that experience.
+
+So whether you're a casual moviegoer or a die-hard cinephile, we invite you to join us on this journey through the wonderful world of cinema. Welcome to our movie website, and enjoy the show!</p>
+    </div>
+  )
+} 
