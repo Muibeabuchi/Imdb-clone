98ded7175720ac5c676d7679615256622ed2a457
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-09 09:11:20 -0700

    fixed initial favorites number error

--- imdb-clone/src/components/FavoriteLink.jsx
+++ imdb-clone/src/components/FavoriteLink.jsx
@@ -2,14 +2,26 @@
 
 import { useFavorites } from "@/store"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 export default function FavoriteLink() {
     const favorites = useFavorites(state=> state.favorites)
     const favNum = favorites?.length
+
+    const [mounted,setMounted] =useState(false);
+    useEffect(()=>{
+      setMounted(true);
+    },[])
+
+    if(!mounted) return;
   return (
     <div className="flex items-center gap-x-[0.5]">
         <Link href='/favorites' className='hover:text-amber-600 mx-2 my-1 font-semibold p-2'>Favorites</Link>
-        <span className="text-lg font-bold bg-white text-amber-700 w-10 h-10 p-0 flex items-center justify-center rounded-full">{favNum || 2}</span>
+        {
+          favNum >0 && (
+            <span className="text-lg font-bold bg-white text-amber-700 w-10 h-10 p-0 flex items-center justify-center rounded-full">{favNum || 0}</span>
+          )
+        }
     </div>
   )
 }
